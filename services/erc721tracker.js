8c0e01092dc729fb9b60ab271acb2e395f6f0833
Aleksei Dmitriev <alexey@ioncloud.ru>
2021-10-05 10:41:17 +0200

    chore: updates

--- services/erc721tracker.js
+++ services/erc721tracker.js
@@ -1,167 +1,170 @@
-require('dotenv').config()
-const axios = require('axios')
-const mongoose = require('mongoose')
-const ethers = require('ethers')
+require("dotenv").config();
+const axios = require("axios");
+const mongoose = require("mongoose");
+const ethers = require("ethers");
 
-const rpcapi = process.env.NETWORK_RPC
-const chainID = parseInt(process.env.NETWORK_CHAINID)
-const ftmScanApiURL = process.env.FTM_SCAN_URL
+const rpcapi = process.env.NETWORK_RPC;
+const chainID = parseInt(process.env.NETWORK_CHAINID);
+const ftmScanApiURL = process.env.FTM_SCAN_URL;
 
-const provider = new ethers.providers.JsonRpcProvider(rpcapi, chainID)
+const provider = new ethers.providers.JsonRpcProvider(rpcapi, chainID);
 
-const NFTITEM = mongoose.model('NFTITEM')
-const ERC721CONTRACT = mongoose.model('ERC721CONTRACT')
-const Like = mongoose.model('Like')
+const NFTITEM = mongoose.model("NFTITEM");
+const ERC721CONTRACT = mongoose.model("ERC721CONTRACT");
+const Like = mongoose.model("Like");
 
-const contractutils = require('./contract.utils')
+const contractutils = require("./contract.utils");
 
-const ftmScanApiKey = process.env.FTM_SCAN_API_KEY
-const validatorAddress = process.env.VALIDATORADDRESS
-const limit = 99999999999999
+const ftmScanApiKey = process.env.FTM_SCAN_API_KEY;
+const validatorAddress = process.env.VALIDATORADDRESS;
+const limit = 99999999999999;
 
 const toLowerCase = (val) => {
-  if (val) return val.toLowerCase()
-  else return val
-}
+  if (val) return val.toLowerCase();
+  else return val;
+};
 
-const loadedContracts = new Map()
+const loadedContracts = new Map();
 
-const bannedCollections = new Map()
+const bannedCollections = new Map();
 
 const isBannedCollection = async (contractAddress) => {
-  let isBanned = bannedCollections.get(contractAddress)
-  if (isBanned) return true
+  let isBanned = bannedCollections.get(contractAddress);
+  if (isBanned) return true;
   try {
     let contract_721 = await ERC721CONTRACT.findOne({
       address: contractAddress,
       isAppropriate: false,
-    })
+    });
     if (contract_721) {
-      bannedCollections.set(contractAddress, true)
-      return true
+      bannedCollections.set(contractAddress, true);
+      return true;
     } else {
-      bannedCollections.set(contractAddress, false)
-      return false
+      bannedCollections.set(contractAddress, false);
+      return false;
     }
   } catch (error) {
     console.error("Error while performing isBannedCollection: ", error);
-    return false
+    return false;
   }
-}
+};
 const removeLike = async (contractAddress, tokenID) => {
   try {
     await Like.remove({
       contractAddress: contractAddress,
       tokenID: tokenID,
-    })
+    });
   } catch (error) {
     console.error("Error while performing removeLike: ", error);
   }
-}
+};
 
 const trackerc721 = async (begin, end) => {
   try {
-    let request = `${ftmScanApiURL}api?module=account&action=tokennfttx&address=${validatorAddress}&startblock=${begin}&endblock=${end}&sort=asc&apikey=${ftmScanApiKey}`
-    let result = await axios.get(request)
-    let tnxs = result.data.result
+    let request = `${ftmScanApiURL}api?module=account&action=tokennfttx&address=${validatorAddress}&startblock=${begin}&endblock=${end}&sort=asc&apikey=${ftmScanApiKey}`;
+    let result = await axios.get(request);
+    let tnxs = result.data.result;
 
     console.log("start:", start);
     console.log("end: ", end);
 
     console.log(`found ${tnxs?.length} new transactions...`);
-  
 
-    if (!tnxs || tnxs.length == 0) return end
-    else {
-      let promise = tnxs.map(async (tnx) => {
-        let to = toLowerCase(tnx.to)
-        let tokenID = parseInt(tnx.tokenID)
-        let contractAddress = toLowerCase(tnx.contractAddress)
-
-        let nft = await NFTITEM.findOne({
-          contractAddress: contractAddress,
-          tokenID: tokenID,
-        })
-        if (nft) {
-          console.log(`token exists already ${contractAddress} ${tokenID}`)
-          if (to == validatorAddress) {
-            await removeLike(contractAddress, tokenID)
-            await nft.remove()
-          }
-          if (nft.owner != to) {
-            nft.owner = to
-            let now = Date.now()
-            try {
-              if (nft.createdAt > now) nft.createdAt = now
-            } catch (error) {
-              console.error("Error while performing nft.createdAt: ", error);
-
-            }
-            await nft.save()
-          }
-        } else {
-          if (to == validatorAddress) {
-            await removeLike(contractAddress, tokenID)
-          } else {
-            let sc = loadedContracts.get(contractAddress)
-            if (!sc) {
-              sc = contractutils.loadContractFromAddress(contractAddress)
-              loadedContracts.set(contractAddress, sc)
-            }
-            let tokenURI = await sc.tokenURI(tokenID)
-            // if (tokenURI.startsWith('https://')) {
-            let tokenName = '.'
-            let imageURL = '.'
-            try {
-              let metadata = await axios.get(tokenURI)
-              tokenName = metadata.data.name
-              imageURL = metadata.data.image
-                ? metadata.data.image
-                : metadata.data
-            } catch (error) {
-            }
-            let newTk = new NFTITEM()
-            newTk.contractAddress = contractAddress
-            newTk.tokenID = tokenID
-            newTk.name = tokenName
-            newTk.tokenURI = tokenURI
-            newTk.imageURL = imageURL
-            newTk.owner = to
-            newTk.createdAt = new Date(parseInt(tnx.timeStamp) * 1000)
-            let isBanned = await isBannedCollection(contractAddress)
-            newTk.isAppropriate = !isBanned
-            await newTk.save()
-            console.log(`new token of ${contractAddress}, ${tokenID} saved`)
-            // }
-          }
-        }
-      })
-      // await Promise.all(promise)
+    if (!tnxs || tnxs.length == 0) {
+      return end;
     }
-    return end
+    for (const i = 0; i < tnxs.length; i++) {
+      const tnx = tnxs[i];
+      let to = toLowerCase(tnx.to);
+      let tokenID = parseInt(tnx.tokenID);
+      let contractAddress = toLowerCase(tnx.contractAddress);
+
+      let nft = await NFTITEM.findOne({
+        contractAddress: contractAddress,
+        tokenID: tokenID,
+      });
+      if (nft) {
+        console.log(`token exists already ${contractAddress} ${tokenID}`);
+        if (to == validatorAddress) {
+          await removeLike(contractAddress, tokenID);
+          await nft.remove();
+        }
+        if (nft.owner != to) {
+          nft.owner = to;
+          let now = Date.now();
+          try {
+            if (nft.createdAt > now) nft.createdAt = now;
+          } catch (error) {
+            console.error("Error while performing nft.createdAt: ", error);
+          }
+          await nft.save();
+        }
+      } else {
+        if (to == validatorAddress) {
+          await removeLike(contractAddress, tokenID);
+        } else {
+          let sc = loadedContracts.get(contractAddress);
+          if (!sc) {
+            sc = contractutils.loadContractFromAddress(contractAddress);
+            loadedContracts.set(contractAddress, sc);
+          }
+          let tokenURI = await sc.tokenURI(tokenID);
+          // if (tokenURI.startsWith('https://')) {
+          let tokenName = ".";
+          let imageURL = ".";
+          try {
+            let metadata = await axios.get(tokenURI);
+            tokenName = metadata.data.name;
+            imageURL = metadata.data.image
+              ? metadata.data.image
+              : metadata.data;
+          } catch (error) {}
+          let newTk = new NFTITEM();
+          newTk.contractAddress = contractAddress;
+          newTk.tokenID = tokenID;
+          newTk.name = tokenName;
+          newTk.tokenURI = tokenURI;
+          newTk.imageURL = imageURL;
+          newTk.owner = to;
+          newTk.createdAt = new Date(parseInt(tnx.timeStamp) * 1000);
+          let isBanned = await isBannedCollection(contractAddress);
+          newTk.isAppropriate = !isBanned;
+          await newTk.save();
+          console.log(`new token of ${contractAddress}, ${tokenID} saved`);
+        }
+      }
+    }
+
+    return end;
   } catch (error) {
     console.error("Error while performing trackerc721: ", error);
     // console.log(error)
+    return start;
   }
-}
+};
 
-let start = process.env.START_BLOCKNUMBER
+let start = process.env.START_BLOCKNUMBER;
 
 const trackAll721s = async () => {
   const func = async () => {
     try {
-      let currentBlockHeight = await provider.getBlockNumber()
-      const end = parseInt(start, 10) + parseInt(process.env.BLOCKS_PER_SEARCH || 5000, 10);
-      start = await trackerc721(start, currentBlockHeight > end ? end : currentBlockHeight)
+      let currentBlockHeight = await provider.getBlockNumber();
+      const end =
+        parseInt(start, 10) +
+        parseInt(process.env.BLOCKS_PER_SEARCH || 5000, 10);
+      start = await trackerc721(
+        start,
+        currentBlockHeight > end ? end : currentBlockHeight
+      );
 
       setTimeout(async () => {
-        await func()
-      }, 500)
+        await func();
+      }, 500);
     } catch (error) {
       console.error("Error while performing trackAll721s: ", error);
     }
-  }
-  await func()
-}
+  };
+  await func();
+};
 
-module.exports = trackAll721s
+module.exports = trackAll721s;
